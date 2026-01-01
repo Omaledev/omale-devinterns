e6374ea889f1c133ea1f2dd8c99f7f38664e2f30
Omaledev <omaleomale310@gmail.com>
2026-01-01 07:49:46 +0100

    Create a Dockerfile and .dockerignore for hosting

--- sms/app/Providers/AppServiceProvider.php
+++ sms/app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\View;
 use App\Models\AcademicSession;
 use App\Models\Term;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
         // This is to override the default auth routes
         $this->configureAuthRoutes();
         
