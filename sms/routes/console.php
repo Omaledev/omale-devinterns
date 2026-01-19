42422dc8d07745da23e6e1dcdd46123fb52070e5
Omaledev <omaleomale310@gmail.com>
2026-01-19 11:46:35 +0100

    Trying to setting up backup and maintenance.

--- sms/routes/console.php
+++ sms/routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Clean up old backups daily at 1:00 AM
+Schedule::command('backup:clean')->daily()->at('01:00');
+
+// Run a new backup daily at 1:30 AM
+Schedule::command('backup:run')->daily()->at('01:30');
